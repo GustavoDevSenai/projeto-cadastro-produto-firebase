185e7bc354c3c5a51373eb8909b31f8b78e62e69
Your Name <you@example.com>
2026-06-15 14:23:33 -0300

    alteracoes iniciais login

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -1,7 +1,45 @@
+"use client"
+
+import { useState } from "react"
+
 export default function Login(){
 
-    
+    const [email,setEmail] = useState("")
+    const [senha,setSenha] = useState("")
+
+
+    async function entrar(params) {
+        console.log("Entrou!")
+    }
+
+
     return(
 
+        
+        <>
+            <h1>Login</h1>
+
+        <input 
+        placeholder="Digite seu email..."
+        value={email}
+        onChange={(e)=> setEmail(e.target.value)}
+        />
+
+        <input 
+        placeholder="Digite seu email..."
+        value={senha}
+        onChange={(e)=> setSenha(e.target.value)}
+        />
+
+        <button onClick={entrar}>
+            Entrar
+        </button>
+
+        </>
+
+        
+
+        
+        
     )
 }
